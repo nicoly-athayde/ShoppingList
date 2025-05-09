6edc399da51297baba29d13f5e9e558acad61b51
nicoly-athayde <nicolyathayde4@gmail.com>
2025-05-09 12:24:55 -0300

    Continuando

--- ShoppingList/app/home.jsx
+++ ShoppingList/app/home.jsx
@@ -1,23 +1,38 @@
-import { ImageBackground, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import React from 'react'
-
+import React from 'react';
 
 export default function Home() {
-    return (
-        <SafeAreaView style={{ flex: 1}}>
-          <ImageBackground
-            source={require('../assets/background.jpg')}
-            style={{ flex: 1, justifyContent: 'flex-start'}}
-            resizeMode='repeat'
-          >
-            <View style={styles.header}>
-              <Text style={styles.title}>Lista de Produtos</Text>
-              <Ionicons name="trash" size={32} color="#fff" />
-            </View>
-          </ImageBackground>
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground
+        source={require('../assets/background.jpg')}
+        style={{ flex: 1, justifyContent: 'flex-start' }}
+        resizeMode="repeat"
+      >
+        <View style={styles.header}>
+          <Text style={styles.title}>Lista de Produtos</Text>
+          <Ionicons name="trash" size={32} color="#fff" />
+        </View>
+
+        <View style={{ flex: 1 }}></View>
+
+        <View style={styles.footer}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              placeholder="Digite o nome do produto"
+              placeholderTextColor="#fff"
+              color="#fff"
+              fontSize={18}
+            />
+          </View>
+          <TouchableOpacity style={styles.iconContainer}>
+            <Ionicons name="add" size={36} color="#fff" />
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -34,7 +49,36 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 26,
     fontWeight: 'bold',
-    color: '#fff'
-
-  }
-})
+    color: '#fff',
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#000000c0',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  inputContainer: {
+    backgroundColor: '#000',
+    elevation: 40,
+    flex: 1,
+    height: 50,
+    marginVertical: 20,
+    borderRadius: 30,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+  },
+  iconContainer: {
+   height: 50,
+   width: 30,
+   backgroundColor: '#000',
+   borderRadius: 25,
+   elevation: 40,
+   justifyContent: 'center',
+   alignItems: 'center',
+  },
+});
